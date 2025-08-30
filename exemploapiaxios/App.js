189c6226d2153011d81d2d15f7e66ea27698d037
BrunoDuranTeodoro <duranteodoro20@gmail.com>
2025-08-29 21:11:38 -0300

    aulacompleta

--- exemploapiaxios/App.js
+++ exemploapiaxios/App.js
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from "react";
+import { View, Button, Image, StyleSheet } from "react-native";
+import api from "./src/services/api";
+import { ScrollView } from "react-native";
 
 export default function App() {
+  const [cats, setCats] = useState([]);
+
+  useEffect(()=>{
+    buscar();
+  },[])
+
+  async function buscar() {
+    try {
+      const result = await api.get('https://api.thecatapi.com/v1/images/search?limit=10');
+      console.log(result.data);
+      setCats(result.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const _render = () => {
+    return cats.map((item, index) => (
+      <View style={styles.card} key={index}>
+        <Image
+          source={{ uri: item.url }}
+          style={{ width: 500, height: 500 }}
+        />
+      </View>
+    ));
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button 
+        title="Clique aqui"
+        onPress={buscar}
+      />
+      <ScrollView>
+        {_render()}
+      </ScrollView>
+      
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  card: {
+    marginBottom: 20,
     alignItems: 'center',
     justifyContent: 'center',
   },
+  container:{
+    flex:1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 50
+  }
 });
